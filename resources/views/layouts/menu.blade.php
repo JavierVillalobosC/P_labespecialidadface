8b766698a7d183b5feb2748f9a3ed59b700f9042
Sebasaraneda <sebastian.araneda1901@alumnos.ubiobio.cl>
2023-01-26 18:17:36 -0300

    Usuarios, Roles, Permisos, y dashboard

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,14 @@
+<li class="side-menus {{ Request::is('dash') ? 'active' : '' }}">
+    <a class="nav-link" href="/dash">
+        <i class=" fas fa-building"></i><span>Dashboard</span>
+    </a>
+    <a class="nav-link" href="/usuarios">
+        <i class=" fas fa-building"></i><span>Usuarios</span>
+    </a>
+    <a class="nav-link" href="/roles">
+        <i class=" fas fa-building"></i><span>Roles</span>
+    </a>
+    <a class="nav-link" href="/blogs">
+        <i class=" fas fa-building"></i><span>Blogs</span>
+    </a>
+</li>
